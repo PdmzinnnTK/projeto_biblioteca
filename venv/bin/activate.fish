3436798e2e78a4952c68cba7c1bcaea7721de1da
Allan <allan.bento.santos@escola.pr.gov.br>
2026-09-21 09:40:37 -0300

    Emprestimo

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /tmp/guest-chcsey/Documentos/chapa/projeto_biblioteca/venv
+set -gx VIRTUAL_ENV /tmp/guest-71uxhl/Documentos/chapa/projeto_biblioteca/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
